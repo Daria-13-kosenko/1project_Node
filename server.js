bf801be5b3bbad62a6d221194977eb771b4403e2
Daria_Kosenko <dasha.milkaaa@gmail.com>
2026-03-23 19:35:24 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './db/index.js'
+import authRouter from './routes/auth.js'
+
+dotenv.config()
+const app = express()
+
+const HOST = process.env.HOST || 'localhost'
+const PORT = process.env.PORT || 3333
+app.use(express.json())
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at ${HOST}:${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.error('Failed to start server', error)
+  })
+
+app.use('/', authRouter)
