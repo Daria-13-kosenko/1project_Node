c2c35a7066a2e9aa8f63645a1a3f4fde20acd685
Daria_Kosenko <dasha.milkaaa@gmail.com>
2026-03-23 19:55:29 +0100

    implemented registred and trying made login

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,85 @@
 import { Router } from 'express'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import express from 'express'
+import { getDb } from '../db/index.js'
 
-const router = Router()
+const router = express.Router()
 
-router.post('/register', (req, res) => {
-  res.send('User registred')
+dotenv.config()
+
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+      return res.status(400).json({
+        message: 'Username and password are required!',
+      })
+    }
+
+    const db = getDb()
+    const usersCollection = db.collection('users')
+
+    const existingUser = await usersCollection.findOne({ username })
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: 'User already exist',
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = {
+      username,
+      password: hashedPassword,
+      createdAt: new Date(),
+    }
+
+    const result = await usersCollection.insertOne(newUser)
+    res.status(201).json({
+      message: 'User registred successfully',
+      userId: result.inserId,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server error',
+      error: error.message,
+    })
+  }
 })
 
-router.post('/login', (req, res) => {
-  res.send('User logged in!')
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+      return res.status(400).json({
+        message: 'Username and password are required',
+      })
+    }
+
+    const db = getDb()
+    const usersCollection = db.collection('users')
+
+    const user = await usersCollection.findOne({ username })
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      })
+    }
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        message: 'Invalid password',
+      })
+    }
+
+    const token
+  } catch (error) {}
 })
 
 export default router
